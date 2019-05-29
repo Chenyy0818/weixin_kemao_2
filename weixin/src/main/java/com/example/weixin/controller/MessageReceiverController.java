6957e4e7cf2e00a2e59241e3ec53de998c72c973
Cyy <Cyy@DESKTOP-GCLSHP7>
2019-05-29 20:37:36 +0800

    此次

--- weixin/src/main/java/com/example/weixin/controller/MessageReceiverController.java
+++ weixin/src/main/java/com/example/weixin/controller/MessageReceiverController.java
@@ -13,39 +13,31 @@ import com.fkjava.weixin.domain.InMessage;
 import com.fkjava.weixin.domain.text.TextInMessage;
 import com.fkjava.weixin.service.MessageConvertHelper;
 
-@RestController 
+@RestController
 @RequestMapping("/kemao_2/message/receiver")
 public class MessageReceiverController {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
-	
-	
-@GetMapping
-	public String echo(
-		@RequestParam("signature")	String signature,
-		@RequestParam("timestamp")	String timestamp,
-		@RequestParam("nonce")	String nonce,
-		@RequestParam("echostr")	String echostr
-			
-			
-			)	 {
-	return echostr;
-	
-}
-@PostMapping
-public String onMessage(@RequestBody String xml) {
-	
-	
-	LOG.trace("收到的消息原文:\n{}\n------------------*,xml");
-	
 
-	InMessage inMessage = MessageConvertHelper.convert(xml);
-	
-	LOG.debug("转换后的消息对象\n{}\n",inMessage);
-	
-	
-	return"success";
-}
+	@GetMapping
+	public String echo(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp,
+			@RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr
 
+	) {
+		return echostr;
+
+	}
+
+	@PostMapping
+	public String onMessage(@RequestBody String xml) {
+
+		LOG.trace("收到的消息原文:\n{}\n------------------*,xml");
+
+		InMessage inMessage = MessageConvertHelper.convert(xml);
+
+		LOG.debug("转换后的消息对象\n{}\n", inMessage);
+
+		return "success";
+	}
 
 }
