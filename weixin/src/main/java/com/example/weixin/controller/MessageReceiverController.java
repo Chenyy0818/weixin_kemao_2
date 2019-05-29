fe2863828574fc10800af2d608570ec62c62ce03
Cyy <Cyy@DESKTOP-GCLSHP7>
2019-05-29 18:48:46 +0800

    接受微信公众号发送给我们的消息

--- weixin/src/main/java/com/example/weixin/controller/MessageReceiverController.java
+++ weixin/src/main/java/com/example/weixin/controller/MessageReceiverController.java
@@ -1,13 +1,21 @@
 package com.example.weixin.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController 
 @RequestMapping("/kemao_2/message/receiver")
 public class MessageReceiverController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
+	
+	
 @GetMapping
 	public String echo(
 		@RequestParam("signature")	String signature,
@@ -20,4 +28,13 @@ public class MessageReceiverController {
 	return echostr;
 	
 }
+@PostMapping
+public String onMessage(@RequestBody String xml) {
+	
+	
+	LOG.trace("收到的消息原文:\n{}\n------------------*,xml");
+	return"success";
+}
+
+
 }
